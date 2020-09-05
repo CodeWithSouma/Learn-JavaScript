c3c0f13d0980949edbeff1b46acc5b859629b596
Souma <souma.hitech@gmail.com>
2020-09-05 10:22:46 +0530

    Variable declair

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
- //This is my first java script code
- console.log('Hello World.');
+//  //This is my first java script code
+//  console.log('Hello World.');
+
+//variable declairation
+// 1.They can't be a reserved keyword
+// 2. They should be meaningfull
+// 3.They can't be start with number(1name)
+// 4.They can't be store space or hyphen(-)
+// 5.You should use camel case notation for variable declair
+// 6.They are case sensitive
+let name = 'Souma';
+console.log(name);
