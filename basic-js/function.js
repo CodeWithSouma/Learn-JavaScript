37c2dd20a6841ee83590b6bdd40485869fdf6a33
Souma <souma.hitech@gmail.com>
2020-09-05 11:59:39 +0530

    complete function

---
+++ basic-js/function.js
@@ -0,0 +1,18 @@
+//function declair 
+//parameteries function
+//perfoming a task
+function greet(firstName,lastName){
+    console.log('Hello '+firstName+' '+lastName);
+}
+
+//calculate a value
+function square(number) {
+    return number * number;
+}
+
+//function call
+//we pass souma and dey argument
+greet('Souma','Dey');
+
+console.log(square(5));
+console.log(console);
