e41c8a8e598ed9fc358e826c7009465231d2edc3
Souma <souma.hitech@gmail.com>
2020-09-05 12:29:34 +0530

    complete Assignment Operators

---
+++ basic-js/assignment.js
@@ -0,0 +1,19 @@
+// Assignment Operators
+let x = 10;
+x+=5;
+console.log(x);
+
+x*=5;
+console.log(x);
+
+x-=5;
+console.log(x);
+
+x/=7;
+console.log(x);
+
+x**=2;
+console.log(x);
+
+x%=10;
+console.log(x);
