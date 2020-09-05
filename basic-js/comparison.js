024e5c0fd1aad736318e96a4fef4ed30b668918b
Souma <souma.hitech@gmail.com>
2020-09-05 12:41:11 +0530

    complete comparison operator

---
+++ basic-js/comparison.js
@@ -0,0 +1,12 @@
+// Comparison Operators
+let x = 1;
+
+// Relational Operators
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 0);
+console.log(x <= 1);
+
+// Equallity Operator
+console.log(x === 1);
+console.log(x !== 1);
