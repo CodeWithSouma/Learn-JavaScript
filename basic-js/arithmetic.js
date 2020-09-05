7f3b0566fc7cd44afcd2db4af688ef79bda2f93f
Souma <souma.hitech@gmail.com>
2020-09-05 12:22:00 +0530

    complete Arithmetic operator.

---
+++ basic-js/arithmetic.js
@@ -0,0 +1,29 @@
+
+let x = 10;
+let y = 3;
+// Arithmetic Operators
+//1.Addition
+//2.Subtraction
+//3.Multiplication
+//4.Divition
+//5.Remainder
+//6.exponant/ power
+//7.Increment
+//8.Decrement
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+// Increment(++)
+console.log(++x);//preincrement
+console.log(x++);//postincrement
+console.log(x);
+
+// Decrement(--)
+console.log(--x);//preincrement
+console.log(x--);//postdecrement
+console.log(x);
