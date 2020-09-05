30225f8966f13bc9c3552ed2e2ef3bc31f76bc5c
Souma <souma.hitech@gmail.com>
2020-09-05 12:48:58 +0530

    equality operator

---
+++ basic-js/equality.js
@@ -0,0 +1,7 @@
+// Strict Equality operator(Same type and same value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Lose Equality Operator(type not matter but same value compaire)
+console.log(1 == 1);
+console.log('1' == 1);
