5e175633f2013bde5027adcd01612565cec0c244
Souma <souma.hitech@gmail.com>
2020-09-05 21:01:34 +0530

    complete while loop.

---
+++ basic-js/while-loop.js
@@ -0,0 +1,8 @@
+// while loop
+//print all odd number between 1 to 5 using while loop
+let i = 1;
+console.log('Printing all odd number between 1 to 5:');
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
