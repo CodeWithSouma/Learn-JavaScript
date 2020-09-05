f465af3ccf7a6937be3d10f131df55b883b2d724
Souma <souma.hitech@gmail.com>
2020-09-05 15:44:20 +0530

    complete swapping demo

---
+++ basic-js/operators-precedence.js
@@ -0,0 +1,7 @@
+// Operators Precedence
+// basic list 
+// 1. * / %
+// 2. + -
+// if you need more information you can check mdn docs
+let x = 2 + 3 * 4;  
+console.log(x);
---
+++ basic-js/swapping.js
@@ -0,0 +1,11 @@
+// Swapping Variables
+let a = 'red';
+let b = 'blue';
+
+// swapping a nd b each other
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
