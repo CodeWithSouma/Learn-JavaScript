553de82c1108c5fb6ff2431f3fe2c98264b4e161
Souma <souma.hitech@gmail.com>
2020-09-05 11:01:49 +0530

    complete object declairation.

---
+++ person.js
@@ -0,0 +1,14 @@
+// person object 
+let person = {
+    name:'Souma',
+    age:22
+};
+
+//access object variable 
+// dot notation
+person.name = 'Soumadip Dey';
+
+// bracket notation or destructure
+person['name'] = 'Souma'
+
+console.log(person.name);
