718bb5610c04e53448760db52d217dd392128dcd
Souma <souma.hitech@gmail.com>
2020-09-06 21:24:11 +0530

    complete math object.

---
+++ basic-js/math.js
@@ -0,0 +1,16 @@
+// Math Built in object of java script
+
+// Math.random() genarate random number 
+//Math.floor() convert floating point number to nearest int bellow the original number
+console.log('Radom Number: '+Math.floor(Math.random()*10));
+
+// Math.abs() returns absolute value of a number
+console.log('Aabsolute value of -5: '+Math.abs(-5));
+
+//Math.round() returns round figure of a number
+console.log('Round .5: '+Math.round(.5));
+
+// Math.max() return maximum value
+console.log('Maximum number between 1,2,3,4,5: '+Math.max(1,2,3,4,5));
+
+// you can checkout MDN for more information
