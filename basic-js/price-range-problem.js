41b29779016824c25f0c4b1a47c889f1c298adb7
Souma <souma.hitech@gmail.com>
2020-09-07 01:57:22 +0530

    complete price-range problem.

---
+++ basic-js/price-range-problem.js
@@ -0,0 +1,14 @@
+// price range problem 
+let priceRanges = [
+    {lable:'$',tooltip:'Inexpensive',minPerPerson:0,maxPerPerson:10},
+    {lable:'$$',tooltip:'Moderate',minPerPerson:11,maxPerPerson:20},
+    {lable:'$$$',tooltip:'Expensive',minPerPerson:21,maxPerPerson:50}
+]
+
+let restaurants = [
+    {name:'FoodG',avgPerPerson:5},
+    {name:'5 star',avgPerPerson:10},
+    {name:'Taj',avgPerPerson:30},
+]
+
+// now we can filter restaurent name by price range 
