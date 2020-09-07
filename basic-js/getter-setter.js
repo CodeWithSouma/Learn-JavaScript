a53228ffd7cfffda4fe9e8b7ead4dbfad6c833d9
Souma <souma.hitech@gmail.com>
2020-09-08 00:22:29 +0530

    add error handeling in getter-setter.

--- basic-js/getter-setter.js
+++ basic-js/getter-setter.js
@@ -14,14 +14,26 @@ const person = {
     lastName:'Dey',
     get fullName(){return `${person.firstName} ${person.lastName}`},
     set fullName(value){
+        // add error handeling
+        if(typeof value !== 'string') 
+            throw new Error('Value is not a string.');//through exeception;
+
        const parts = value.split(' ');
+        if(parts.length !== 2)
+            throw new Error('Enter a first and last name.');//through exception
+
        this.firstName = parts[0];
        this.lastName = parts[1];
 
     }
 }
 
-person.fullName = 'Soumadip Dey'
+// person.fullName = 'Soumadip Dey'
+try {
+    person.fullName = true;
+} catch (error) {
+    console.log(error);
+}
 
 console.log(person);
-console.log(person.fullName);
+// console.log(person.fullName);
