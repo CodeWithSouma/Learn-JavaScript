9746354f79b58b82d5950457c0dafc1a5af4ab16
Souma <souma.hitech@gmail.com>
2020-09-09 23:43:27 +0530

    complete object creation using object literal.

---
+++ OOP-JavaScript/create-object.js
@@ -0,0 +1,14 @@
+// create a object using object literal
+
+const circle = {
+    radious:1,
+    location:{
+        x:1,
+        y:1
+    },
+    draw:function(){//draw is a method
+        console.log('Draw');
+    }
+}
+
+circle.draw();
