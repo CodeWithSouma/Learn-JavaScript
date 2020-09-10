ee5afe7cbe5b702d5a1fe6d580aa558bb13d0354
Souma <souma.hitech@gmail.com>
2020-09-10 23:28:53 +0530

    complete prototype demo4

---
+++ OOP-JavaScript/prototype-demo4.js
@@ -0,0 +1,46 @@
+// Prototype vs instance member
+function Circle(radious) {
+
+    //instance member 
+    this.radious = radious;
+
+    // it is the common property of all Circle so we cut it and put it into prototype
+    // this.draw = function () { 
+    //     console.log('Draw');  
+    // }
+
+    this.move = function () {
+        // we can also call prototypical method from here
+        this.draw();
+        console.log('Move');
+    }
+}
+
+//when we create circle object then every time 
+// circle object gets radious and draw method
+// it is the waste of memory we can extract draw method and
+// put into base object let see
+
+// Circle.prototype === c1.__proto__ it referencing the same object 
+// that is circle base we know prototype is a nested object of Circle constructor
+
+// prototype memeber
+Circle.prototype.draw = function () { 
+    console.log('Draw');  
+    // we can call instance method from here
+    // this.move();
+}
+
+
+const c1 = new Circle(1);
+const c2 = new Circle(1);
+
+// becauses of the prototypcal inheritence we can access draw method
+c1.draw();
+
+// we can override the toString method
+Circle.prototype.toString = function () {
+    return 'Circle with radious '+this.radious;
+}
+
+console.log(c1.toString());;
